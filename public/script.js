726d10a7a9a610a9c874b321f3b567076b6b33a6
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-08-10 00:45:28 +0100

    added items to the hamburger menu

--- public/script.js
+++ public/script.js
@@ -1,6 +1,11 @@
 // selecting elements
 const sunIcon = document.querySelector("#sun-icon")
 const moonIcon = document.querySelector("#moon-icon")
+const sunIcon1 = document.querySelector("#sun-icon1")
+const moonIcon1 = document.querySelector("#moon-icon1")
+const sunIcon2 = document.querySelector("#sun-icon2")
+const moonIcon2 = document.querySelector("#moon-icon2")
+
 const allPage = document.querySelector("html")
 
 
@@ -13,6 +18,19 @@ sunIcon.addEventListener("click", () => {
 moonIcon.addEventListener("click", () => {
     allPage.classList.toggle("dark")
 })
+sunIcon1.addEventListener("click", () => {
+    allPage.classList.toggle("dark")
+})
+moonIcon1.addEventListener("click", () => {
+    allPage.classList.toggle("dark")
+})
+sunIcon2.addEventListener("click", () => {
+    allPage.classList.toggle("dark")
+})
+moonIcon2.addEventListener("click", () => {
+    allPage.classList.toggle("dark")
+})
+
 
 
 // using animate on scroll
