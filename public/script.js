832c42ddba990f453c65560524354121a7971916
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-08-11 00:45:26 +0100

    added scroll to top button and added modal appears when clicking on ch33kaboo

--- public/script.js
+++ public/script.js
@@ -31,6 +31,36 @@ moonIcon2.addEventListener("click", () => {
     allPage.classList.toggle("dark")
 })
 
+// for scrolling to top
+// Get the button
+let mybutton = document.getElementById('btn-back-to-top');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
+    mybutton.style.opacity = '100%';
+  } else {
+    mybutton.style.opacity = '0%';
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener('click', backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// display modal when clicking on ch33kaboo
+const ch33kaboo = document.getElementById('ch33kaboo');
+const modal = document.getElementById('my-modal')
+ch33kaboo.addEventListener("click", () => {
+    modal.checked = true;
+})
 
 
 // using animate on scroll
